551851831c705f5b80159766b17e82d939bcd909
SkyWitit <sercan_karaoglu@yahoo.com>
2015-05-23 14:38:06 +0300

    0.72685

---
+++ WestNile/WestNileMain.R
@@ -0,0 +1,43 @@
+library(Metrics)
+library(xgboost)
+library(data.table)
+
+root_dir<-"~/git/RScripts/WestNile/"
+submissionDir<-paste0(root_dir,"submission.csv")
+
+source("~/git/RScripts/WestNile/loadWestNileData.R")
+
+# Set necessary parameter
+param <- list("objective" = "binary:logistic",
+              "eval_metric" = "auc",
+              "eta" = 0.25,
+              "max_depth"=3,
+              "nthread" = 2)
+nrounds<-65
+
+xg.cv = xgb.cv(param=param, data = x1AsMatrix, label = y1, 
+               nfold = 5, nrounds=nrounds)
+
+xg = xgboost(param=param, data = x1AsMatrix, label = y1, nrounds=nrounds)
+# model<-getDeepModel(cbind(x1AsMatrix,y1))
+# h2oPrediction<-as.data.frame(h2o.predict(model,as.h2o(localH2O,x2AsMatrix)))
+xgPrediction<-predict(xg,x2AsMatrix);
+
+## check for a reasonable AUC of the model against unseen data (2011)
+print(paste("xg auc:",auc(y2,xgPrediction)));
+## now fit a new model to all the data, so that our final submission includes information learned from 2011 as well
+xg.cv = xgb.cv(param=param, data = xAsMatrix, label = y, 
+                nfold = 5, nrounds=nrounds)
+xg = xgboost(param=param, data = xAsMatrix, label = y, nrounds=nrounds)
+xgPrediction<-predict(xg,testAsMatrix);
+
+pSubmit<-xgPrediction;
+## look at the predicted distribution (AUC doesn't care about probabilities; just ordering. It's still a good diagnostic)
+summary(pSubmit)
+
+submissionFile<-cbind(test$Id,pSubmit)
+colnames(submissionFile)<-c("Id","WnvPresent")
+options("scipen"=100, "digits"=8)
+write.csv(submissionFile,submissionDir,row.names=FALSE,quote=FALSE)
+
+
---
+++ WestNile/h2oDeepLearning.R
@@ -0,0 +1,50 @@
+library(h2o)
+localH2O <- h2o.init(ip = "localhost", port=54321, nthreads = 1)
+column_bind<-function(data){
+  deepfeatures_layer<-cbind(data[,2:ncol(data)],
+                            data[,1]);
+  deepfeatures_layer;
+}
+extractFeature<-function(layerNumber,model,train){
+  train.hex <- as.h2o(localH2O,train)
+  
+  deepfeatures_layer = h2o.deepfeatures(train.hex, model, layer = layerNumber)
+  deepfeatures_layer<-as.data.frame(deepfeatures_layer);
+  deepfeatures_layer<-as.matrix(deepfeatures_layer);
+  deepfeatures_layer<-matrix(as.numeric(deepfeatures_layer),nrow(deepfeatures_layer),ncol(deepfeatures_layer))
+  deepfeatures_layer;
+}
+
+getDeepModel <- function(train){
+  train.hex <- as.h2o(localH2O,train)
+  
+  predictors <- 1:(ncol(train.hex)-1)
+  response <- ncol(train.hex)
+  
+  model <- h2o.deeplearning(x=predictors,
+                            y=response,
+                            data=train.hex,
+                            classification=T,
+                            activation="RectifierWithDropout",
+                            hidden=c(32,16,8),
+                            hidden_dropout_ratio=c(0.5,0.5,0.5),
+                            input_dropout_ratio=0.05,
+                            epochs=50,
+                            l1=1e-5,
+                            l2=1e-5,
+                            rho=0.99,
+                            epsilon=1e-8,
+                            train_samples_per_iteration=2000,
+                            max_w2=10,
+                            seed=1,
+                            reproducible=T)
+  model;
+}
+getPrediction<-function(model,test){
+  for(i in 1:ncol(test)){
+    test[,i] <- as.numeric(test[,i])
+#   test[,i] <- sqrt(test[,i]+(3/8))
+  }
+  test.hex <- as.h2o(localH2O,test[,1:ncol(test)])
+  prediction<-as.data.frame(h2o.predict(model,test.hex))[,2:10]
+}
